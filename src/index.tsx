19d8b9ec2321ea70902b6ac0038c139dd1808419
Mohit Y <42065586+Mohit21GoJs@users.noreply.github.com>
2020-02-13 02:53:30 +0400

    Add optimizations (#5)
    * remove extra pllugins and add webpack bundle analyzer
    
    * Add lazy and suspense for react

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Main from './main';
 import ErrorBoundary from './components/ErrorBoundary';
 import { IdeaContextProvider } from './contexts/idea';
 
+const Main = React.lazy(() => import('./main'));
 ReactDOM.render(
-    <ErrorBoundary>
-        <IdeaContextProvider>
-            <Main />
-        </IdeaContextProvider>
-    </ErrorBoundary>,
+    <React.Suspense fallback={<h1>Loading data...</h1>}>
+        <ErrorBoundary>
+            <IdeaContextProvider>
+                <Main />
+            </IdeaContextProvider>
+        </ErrorBoundary>
+    </React.Suspense>,
     document.getElementById('content'),
 );

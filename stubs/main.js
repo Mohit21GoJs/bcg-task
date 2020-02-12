6cbef123e0986b1bac03537569b94ab9dad562a3
Sandbox Sync <support@getsandbox.com>
2020-02-12 20:36:10 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -1,7 +1,7 @@
 
 var nowDate = moment();
 var initialIdeas = _.range(50).map(function(val){ 
-    return { id: faker.random.uuid(), title: "Card "+ val, body: "Card #" + val + " Body", created_date: nowDate.subtract(val, 'days')};
+    return { id: faker.random.uuid(), title: "Card "+ val, body: "Card #" + val + " Body", date: nowDate, created_date: nowDate.subtract(val, 'days')};
 })
 
 

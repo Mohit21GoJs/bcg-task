8811fd79b4f258a32fdaebe801a9515fd9fce025
Mohit Y <mohit.developer21@gmail.com>
2020-02-14 18:59:44 +0400

    add demo styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import ErrorBoundary from './components/ErrorBoundary';
 import { IdeaContextProvider } from './contexts/idea';
 
 const Main = React.lazy(() => import('./main'));
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: Arial,Helvetica Neue,Helvetica,sans-serif; 
+  }
+`;
 ReactDOM.render(
     <React.Suspense fallback={<h1>Loading data...</h1>}>
+        <GlobalStyle />
         <ErrorBoundary>
             <IdeaContextProvider>
                 <Main />
--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import { useIdeaContext } from './contexts/idea';
 toast.configure({
     autoClose: 3000,
     draggable: false,
+    position: toast.POSITION.BOTTOM_RIGHT,
 });
 
 const IdeaContainer = styled.div`
@@ -19,18 +20,23 @@ const IdeaContainer = styled.div`
 `;
 
 const Layout = styled.div`
-    & .add-new {
-        position: absolute;
-        top: 10px;
-        right: 0;
-        background-color: green;
-        max-width: 10vw;
+    width: 100%;
+`;
+
+const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    min-height: 70px;
+    align-items: center;
+    background-color: whitesmoke;
+    margin-bottom: 20px;
+    margin-right: 10px;
+    padding-left: 20px;
+    & .heading {
+        font-style: italic;
     }
-    & .reset {
-        position: absolute;
-        bottom: 10px;
-        right: 0;
-        background-color: blue;
+    & .add-new {
+        background-color: green;
         max-width: 10vw;
     }
     & .sort-select {
@@ -122,16 +128,22 @@ const Main: React.FC = () => {
 
     return (
         <Layout>
-            <Select
-                className="sort-select"
-                options={selectOptions}
-                onChange={selectedOption => {
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-                    // @ts-ignore
-                    setSortOption(selectedOption.value);
-                }}
-            />
+            <Header>
+                <h1 className="heading">Ideas Demo App</h1>
+                <Select
+                    className="sort-select"
+                    options={selectOptions}
+                    onChange={selectedOption => {
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+                        // @ts-ignore
+                        setSortOption(selectedOption.value);
+                    }}
+                />
 
+                <Button className="add-new" onClick={handleAddNewIdea}>
+                    Add New
+                </Button>
+            </Header>
             <IdeaContainer>
                 {displayIdeas.map(idea => (
                     <Tile
@@ -145,9 +157,6 @@ const Main: React.FC = () => {
                     />
                 ))}
             </IdeaContainer>
-            <Button className="add-new" onClick={handleAddNewIdea}>
-                Add New
-            </Button>
         </Layout>
     );
 };
